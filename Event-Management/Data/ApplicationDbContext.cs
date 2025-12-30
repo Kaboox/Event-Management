25861af45c952ba264b903077adbca18c6c7e541
Kacper <kabox9@gmail.com>
2025-12-30 21:39:53 +0100

    Dodaj pliki projektów.

---
+++ Event-Management/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Event_Management.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
